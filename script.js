68ffce14ee2828f1de6deeb02881445ec1b501dc
MarsalS1050 <marsalsoren1050@gmail.com>
2023-09-01 23:39:00 +0530

    added first page

--- script.js
+++ script.js
@@ -9,3 +9,56 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+// javacript for more or less
+function myFunction1() {
+
+    var dots1 = document.getElementById(`dots1`);
+    var moreText1 = document.getElementById(`more1`);
+    var btnText1 = document.getElementById(`myBtn1`);
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText1.innerHTML = "Read more";
+      moreText1.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText1.innerHTML = "Read less";
+      moreText1.style.display = "inline";
+    }
+    
+  }
+function myFunction2() {
+
+    var dots1 = document.getElementById(`dots2`);
+    var moreText1 = document.getElementById(`more2`);
+    var btnText1 = document.getElementById(`myBtn2`);
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText1.innerHTML = "Read more";
+      moreText1.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText1.innerHTML = "Read less";
+      moreText1.style.display = "inline";
+    }
+    
+  }
+function myFunction3() {
+
+    var dots1 = document.getElementById(`dots3`);
+    var moreText1 = document.getElementById(`more3`);
+    var btnText1 = document.getElementById(`myBtn3`);
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText1.innerHTML = "Read more";
+      moreText1.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText1.innerHTML = "Read less";
+      moreText1.style.display = "inline";
+    }
+    
+  }
